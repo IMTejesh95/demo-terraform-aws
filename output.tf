09c4f3cc679dfcc4b7c4af4e56173aa53811052b
Tejesh Chauragade <tejesh@TJMAC.local>
2024-10-10 15:28:46 +0530

    modify output

--- output.tf
+++ output.tf
@@ -1,5 +1,8 @@
 output "instances" {
   description = "ID of the EC2 instance"
-  value       = aws_instance
+  value       = {
+    for instance in aws_instance.tf_ec2_1:
+      instance.id => instance.id
+  }
 }
 
