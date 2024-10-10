509b5f18c857ca40a3f249effc4aa1b0cd9c6483
Tejesh Chauragade <tejesh@TJMAC.local>
2024-10-10 14:12:13 +0530

    test

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,12 @@ resource "aws_instance" "tf_ec2_1" {
   ami                  = var.ami
   instance_type        = var.instance_type
   iam_instance_profile = var.ec2_iam_role
+  count = 2
   tags = {
-    Name = "rf-demo-instance"
+    Name = "tf-demo-instance"
     uai  = "UAI617263"
+    demotag = "dummy"
+
   }
+  depends_on = [  ]
 }
